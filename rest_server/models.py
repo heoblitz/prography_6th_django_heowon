7f4485293deebf39d0f5217278946fca43255337
wone2287 <wone2287@gmail.com>
2020-03-01 21:26:11 +0900

    make CRUD method

--- rest_server/models.py
+++ rest_server/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Posts(models.Model):
     id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
--- rest_server/views.py
+++ rest_server/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,7 +8,7 @@ from .models import Posts
 from django.http import JsonResponse
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def posts_list(request):
     # 게시글 리스트 조회
     if request.method == 'GET':
@@ -16,7 +16,16 @@ def posts_list(request):
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
+    # 게시글 생성 
+    elif request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def posts_detail(request, pk):
     try:
         post = Posts.objects.get(pk=pk)
@@ -27,6 +36,20 @@ def posts_detail(request, pk):
     if request.method == 'GET':
         serializer = PostSerializer(post)
         return Response(serializer.data)
+
+    # 해당 PK 게시글 수정
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 해당 PK 게시글 삭제
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 '''
 class PostView(viewsets.ModelViewSet):
